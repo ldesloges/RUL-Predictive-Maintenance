a07db744999fdf316e764858f412d0ab37c91dfd
DESLOGES <ldesloges4@gmail.com>
2026-02-08 18:52:00 +0100

    gr

--- RUL.py
+++ RUL.py
@@ -96,73 +96,78 @@ def data_test_prep(fichier,scaler):
 
     return data_smooth
 
-if __name__ == "__main__":
-    joblib.dump(model, 'model_RUL.pkl')
-    joblib.dump(Myscaler, 'scaler.pkl')
-    joblib.dump(columns, 'features_list.pkl')
-    print("Fichiers .pkl créés avec succès !")
-    #Train
-    data_test=data_test_prep('data/test_FD001.txt',Myscaler)
-    df_RUL=pd.read_csv('data/RUL_FD001.txt',header=None)
-    df_RUL.columns=['true_RUL']
+
+joblib.dump(model, 'model_RUL.pkl')
+joblib.dump(Myscaler, 'scaler.pkl')
+joblib.dump(columns, 'features_list.pkl')
+print("Fichiers .pkl créés avec succès !")
+#Train
+data_test=data_test_prep('data/test_FD001.txt',Myscaler)
+df_RUL=pd.read_csv('data/RUL_FD001.txt',header=None)
+df_RUL.columns=['true_RUL']
 
 
-    columns=[col for col in data_train if 'Capteur' in col]
+columns=[col for col in data_train if 'Capteur' in col]
 
-    X=data_train[columns]
-    y=data_train['RUL']
+X=data_train[columns]
+y=data_train['RUL']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=42)
-    print("Entraînement de l'IA en cours...")
-    model.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=42)
+print("Entraînement de l'IA en cours...")
+model.fit(X_train, y_train)
 
-    X_test=data_test[columns]
-    predictions = model.predict(X_test)
+X_test=data_test[columns]
+predictions = model.predict(X_test)
 
-    # On trouve l'index de la ligne où le Nb_vol est maximum pour chaque moteur
-    last_indices = data_test.groupby('ID_Moteur')['Nb_vol'].idxmax()
-    # On transforme les prédictions en Series avec le même index que data_test
-    preds_series = pd.Series(predictions, index=data_test.index)
-    # On extrait uniquement les prédictions correspondant aux derniers vols
-    final_predictions = preds_series.loc[last_indices]
+# On trouve l'index de la ligne où le Nb_vol est maximum pour chaque moteur
+last_indices = data_test.groupby('ID_Moteur')['Nb_vol'].idxmax()
+# On transforme les prédictions en Series avec le même index que data_test
+preds_series = pd.Series(predictions, index=data_test.index)
+# On extrait uniquement les prédictions correspondant aux derniers vols
+final_predictions = preds_series.loc[last_indices]
 
-    mae = mean_absolute_error(df_RUL['true_RUL'], final_predictions)
-    r2 = r2_score(df_RUL['true_RUL'], final_predictions)
-    print(f"Score R² : {r2:.2f}")
+mae = mean_absolute_error(df_RUL['true_RUL'], final_predictions)
+r2 = r2_score(df_RUL['true_RUL'], final_predictions)
+print(f"Score R² : {r2:.2f}")
 
-    plt.scatter(df_RUL['true_RUL'],final_predictions, alpha=0.5)
-    plt.xlabel('true RUL')
-    plt.ylabel('prédiction du RUL')
-
-
+plt.scatter(df_RUL['true_RUL'],final_predictions, alpha=0.5)
+plt.xlabel('true RUL')
+plt.ylabel('prédiction du RUL')
 
 
 
-    # 1. Récupérer les scores d'importance
-    importances = model.feature_importances_
 
-    # 2. Associer les scores aux noms des colonnes
-    feature_importance_df = pd.DataFrame({
-        'Variable':columns,
-        'Importance': importances
-    })
 
-    # 3. Trier par importance décroissante
-    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+# 1. Récupérer les scores d'importance
+importances = model.feature_importances_
 
-    # 4. Afficher le top 10 dans la console
-    print("Top 10 des colonnes qui font pencher la balance :")
-    print(feature_importance_df.head(10))
+# 2. Associer les scores aux noms des colonnes
+feature_importance_df = pd.DataFrame({
+    'Variable':columns,
+    'Importance': importances
+})
 
-    # 5. Bonus : Le voir en graphique
-    import matplotlib.pyplot as plt
+# 3. Trier par importance décroissante
+feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
 
-    plt.figure(figsize=(10, 6))
-    plt.barh(feature_importance_df['Variable'].head(10), feature_importance_df['Importance'].head(10))
-    plt.gca().invert_yaxis() # Pour avoir la plus importante en haut
-    plt.title("Qu'est-ce qui cause la panne selon l'IA ?")
-    plt.xlabel("Niveau d'importance")
-    plt.show()
+# 4. Afficher le top 10 dans la console
+print("Top 10 des colonnes qui font pencher la balance :")
+print(feature_importance_df.head(10))
+
+# 5. Bonus : Le voir en graphique
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+plt.barh(feature_importance_df['Variable'].head(10), feature_importance_df['Importance'].head(10))
+plt.gca().invert_yaxis() # Pour avoir la plus importante en haut
+plt.title("Qu'est-ce qui cause la panne selon l'IA ?")
+plt.xlabel("Niveau d'importance")
+plt.show()
+
+print("💾 Sauvegarde des fichiers .pkl en cours...")
+joblib.dump(model, 'model_RUL.pkl')
+joblib.dump(Myscaler, 'scaler.pkl')
+joblib.dump(columns_to_use, 'features_list.pkl')
 
 
